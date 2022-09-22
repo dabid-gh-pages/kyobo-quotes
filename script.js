295c1fad92df30ae71793025b367b72af84cc369
Hanalia <davethe@naver.com>
2022-09-23 00:33:36 +0900

    정정

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then((reg) => {
+    navigator.serviceWorker.register("sw.js").then((reg) => {
       // console.log('service worker found', reg);
     });
   });
