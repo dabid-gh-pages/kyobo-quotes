86d8a35f560932d84a17fa101743695f5e811d50
Hanalia <davethe@naver.com>
2022-09-24 22:50:02 +0900

    Updates

--- js/home.js
+++ js/home.js
@@ -2,11 +2,39 @@
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js").then((reg) => {
+    navigator.serviceWorker.register(".././sw.js").then((reg) => {
       // console.log('service worker found', reg);
     });
   });
 }
+
+const parseKyobo = (inputString) => {
+  const quote = inputString.split("\n\n \n\n ")[0].trim();
+  const title = inputString
+    .split("\n\n \n\n ")[1]
+    .split("\n\n")[0]
+    .replace(" 중에서", "");
+
+  return {
+    quote,
+    title,
+    date: new Date().toISOString(),
+  };
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  const parsedUrl = new URL(window.location);
+  // searchParams.get() will properly handle decoding the values.
+  const rawText = parsedUrl.searchParams.get("text");
+  console.log({ rawText });
+  // if rawText not null-> execute function
+  if (rawText) {
+    const parsedQuote = parseKyobo(rawText);
+
+    document.querySelector("#title-input").value = parsedQuote.title;
+    document.querySelector("#quote-input").value = parsedQuote.quote;
+  }
+});
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (evt) => {
@@ -24,13 +52,3 @@ form.addEventListener("submit", (evt) => {
   form.title.value = "";
   form.ingredients.value = "";
 });
-
-// window.addEventListener("DOMContentLoaded", () => {
-//     const parsedUrl = new URL(window.location);
-//     // searchParams.get() will properly handle decoding the values.
-//     const a = "Title shared: " + parsedUrl.searchParams.get("title");
-//     const b = "Text shared: " + parsedUrl.searchParams.get("text");
-//     const c = "URL shared: " + parsedUrl.searchParams.get("url");
-
-//     document.querySelector("#test-div").value = [a, b, c].join("\n");
-//   });
